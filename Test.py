c02cea9bac66b5c9048ddfc9248fcf08f3a1d29d
purifid-water <julianng1110@gmail.com>
2022-11-10 13:30:48 +0700

    Can request docx pdf but exist unreadable data

--- Test.py
+++ Test.py
@@ -1,13 +1,25 @@
 import socket
 
-def readChunk(response, f):
+def readChunk(response, lenHead, f):
+    response = client.recv(lenHead)
     while True:
         response = client.recv(4096)  
         if len(response) == 0:
             break
-        f.write(response)
 
-target_host = "www.web.stanford.edu" 
+        http_response = repr(response)
+        http_response_len = len(http_response)
+        print(http_response)
+        f.write(response)
+    f.close()
+
+# input website name
+#### code here
+print("Enter host address (eg. www.example.com)\n")
+target_host = input('')
+print("Enter extension name (eg. html, pdf, doc...))\n")
+extName = input('')
+
 
 target_port = 80  # create a socket object 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
@@ -15,9 +27,13 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect the client 
 client.connect((target_host,target_port))  
 
+
 # send some data 
 # request = "GET /index.html/ HTTP/1.1\r\nHost:%s\r\n\r\n" % target_host
-request = "GET http://web.stanford.edu/class/cs224w/slides/01-intro.pdf HTTP/1.1\r\nHost:%s\r\n\r\n" % target_host
+request = "GET http://www-net.cs.umass.edu/wireshark-labs/Wireshark_Intro_v8.1.docx HTTP/1.1\r\nHost:%s\r\n\r\n" % target_host
+head  = "HEAD http://www-net.cs.umass.edu/wireshark-labs/Wireshark_Intro_v8.1.docx HTTP/1.1\r\nHost:%s\r\n\r\n" % target_host
+
+
 client.send(request.encode())  
 
 # receive some data 
@@ -27,15 +43,18 @@ client.send(request.encode())
 
 
 
-filename = target_host + '.pdf'
+filename = target_host + '.' + extName
 f = open(filename , "wb");
 # f.write(response)
 response = b''
-readChunk(response, f)
-
-http_response = repr(response)
-http_response_len = len(http_response)
-
+readChunk(response, len(head), f)
+print(head)
+print(len(head))
 
 
-print(http_response)
+client.close()
+
+
+
+
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+www-net.cs.umass.edu
